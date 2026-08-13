80b0c56f68ee81332b3ccd3d9319dd45eaaac559
Quyen Tat <tvq9612@gmail.com>
2026-08-13 12:35:34 +0700

    Wire multi-window manager to live symbols

--- components/orderbook/orderbook-manager.tsx
+++ components/orderbook/orderbook-manager.tsx
@@ -7,19 +7,18 @@ export function OrderBookManager() {
   const { books, order, close, focus } = useOrderBooks()
 
   return (
-    <div className="pointer-events-none fixed inset-0 z-40">
-      <div className="pointer-events-auto">
-        {books.map((b, i) => (
-          <OrderBookPanel
-            key={b.key}
-            stockKey={b.key}
-            index={i}
-            z={40 + order.indexOf(b.key)}
-            onClose={() => close(b.key)}
-            onFocus={() => focus(b.key)}
-          />
-        ))}
-      </div>
+    <div className="pointer-events-none fixed inset-0 z-50">
+      {books.map((book, index) => (
+        <OrderBookPanel
+          key={book.key}
+          stockKey={book.key}
+          symbol={book.symbol}
+          index={index}
+          z={70 + order.indexOf(book.key)}
+          onClose={() => close(book.key)}
+          onFocus={() => focus(book.key)}
+        />
+      ))}
     </div>
   )
 }
